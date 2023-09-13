6cbe9b5a38c217a06501b68bf9767607f2340461
yuni-hutsuka <yuni.wille999@gmail.com>
2023-09-13 14:45:57 +0900

    :construction: wip: 大体できたけどエッジケースに対応できてるかな

--- src/bon_lib/config/init.rs
+++ src/bon_lib/config/init.rs
@@ -1,6 +1,6 @@
 use std::{
     fs::{self, File},
-    io::Write,
+    io::{Result, Write},
     path::{Path, PathBuf},
 };
 
@@ -8,20 +8,28 @@ use home_dir::HomeDirExt;
 
 use crate::bon_lib::utils::path::{CONFIG_DIR, CONFIG_FILE, DEFAULT_CONFIG};
 
-pub fn init() {
-    let config_dir_path: PathBuf = Path::new(CONFIG_DIR).expand_home().unwrap();
-    let config_file_path: PathBuf = Path::new(CONFIG_FILE).expand_home().unwrap();
+pub fn init() -> Result<()> {
+    let config_dir_path: PathBuf = Path::new(CONFIG_DIR)
+        .expand_home()
+        .expect("Something went wrong on expand tilde.");
+    let config_file_path: PathBuf = Path::new(CONFIG_FILE)
+        .expand_home()
+        .expect("Something went wrong on expand tilde.");
 
     if !config_dir_path.is_dir() {
-        fs::create_dir(config_dir_path).unwrap()
+        fs::create_dir(config_dir_path)?
     } else {
-        println!("Config directory is already exist.")
+        println!("Warning: Config directory is already exist.")
     }
 
     if !config_file_path.is_file() {
-        let mut config_file: File = File::create(config_file_path).unwrap();
-        config_file.write_all(DEFAULT_CONFIG.as_bytes()).unwrap();
+        let mut config_file: File = File::create(config_file_path)?;
+        config_file.write_all(DEFAULT_CONFIG.as_bytes())?;
     } else {
-        println!("Config file is already exist.")
+        println!("Warning: Config file is already exist.")
     }
+
+    println!("Initialized config directory ~/.config/bon/");
+
+    Ok(())
 }
--- src/bon_lib/config/mod.rs
+++ src/bon_lib/config/mod.rs
@@ -1,2 +1,2 @@
-pub mod init;
 pub mod edit;
+pub mod init;
